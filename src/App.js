526b4fa1d270348e426fb4eba94fd5923be9f682
Christian Pascua <cristianpascuafdez@gmail.com>
2022-12-13 12:10:03 +0100

    Project completed

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState} from 'react';
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+
+import quotes from './quotes.json';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const colors =[
+	"#00ab00",
+	"#c500bc",
+	"#0b3000",
+	"#0046cc",
+	"#ff6440",
+	"#0033b1",
+	"#ffa06a",
+	"#9693ff",
+	"#002b25",
+	"#ff94f6"
+]
+
+function App() {
+
+  const [randQuote, setRandQuote] = useState(Math.floor(Math.random() * quotes.length));
+  const [randColor, setRandColor] = useState(colors[Math.floor(Math.random() * colors.length)]);
+
+  function handleNewQuote() {
+    setRandQuote(Math.floor(Math.random() * quotes.length));
+    setRandColor(colors[Math.floor(Math.random() * colors.length)])
+  }
+  document.body.style.background= randColor
+  document.body.style.color = randColor
+  return (
+    <div className="App">
+      <h1>Random Quote Machine</h1>
+      <Container id="quote-box" className='container-fluid'>
+        <Row id="text">
+            <p>"{quotes[randQuote].text}"</p>
+        </Row>
+        <Row id="author">
+            <p><strong>{quotes[randQuote].author}</strong></p>
+        </Row>
+        <Row>
+          <Col className='col-xs-6'>
+            <Button id="new-quote" onClick={handleNewQuote}>
+              Get New Quote
+            </Button>
+          </Col>
+          <Col className='col-xs-6'>
+            <a href="twitter.com/intent/tweet" id="tweet-quote">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+</svg>
+            </a>
+          </Col>
+        </Row>
+        
+      </Container>
+
+      
+    </div>
+  );
+}
+
+export default App;
